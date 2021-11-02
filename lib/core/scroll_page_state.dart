88aa9197a9e367e662bec998033409dc00bd85ab
Cameron Dehning <32527538+bedsteler20@users.noreply.github.com>
2021-11-01 19:34:17 -0500

    code splitting

---
+++ lib/core/scroll_page_state.dart
@@ -0,0 +1,100 @@
+import 'package:booklit/booklit.dart';
+import 'package:meta/meta.dart';
+
+/// Base State class used for making infant scroll pages
+/// The first input type is the [Stateful] widget being implemented
+/// The second input type is the type of data that is being fetched
+abstract class ScrollPageState<W extends StatefulWidget, T> extends State<W> {
+  final ScrollController scrollController = ScrollController();
+
+  @nonVirtual
+  Object? error;
+
+  /// Used to trick [fetchData] into thinking thare is 0 [itemsFetched] while refreshing data
+  bool _isRefreshing = false;
+
+  /// used to prevent fetching data when scroll controllor event fires
+  /// and data is already being fetched
+  bool _isFetchingData = false;
+
+  /// The max amount of items that will be returned bu [fetchData]
+  int get querySize;
+
+  /// The var containing all fetched items. Should
+  /// be used somewhere in [build] method
+  List<T> items = [];
+
+  /// The count of items fetched
+  /// use this instead of [items.length]
+  @nonVirtual
+  int get itemsFetched => _isRefreshing ? 0 : items.length;
+
+  /// Indicates that all items have been fetched
+  /// Stops further scrolling from calling [fetchData]
+  bool _isComplete = false;
+
+  /// set to true after first call of [fetchData] in [initState]
+  bool hasInitialItems = false;
+
+  /// should fetch data it based on [itemsFetched]
+  /// returned value will be added to [items]
+  Future<List<T>> fetchData();
+
+  /// Reteaches all data
+  Future<void> refreshData() async {
+    _isFetchingData = true;
+    _isRefreshing = true;
+
+    final fetched = await fetchData();
+
+    _isComplete = fetched.length < querySize;
+    _isRefreshing = false;
+    _isRefreshing = false;
+
+    setState(() => items.addAll(fetched));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Get initial items
+    Future(() async {
+      try {
+        _isFetchingData = true;
+        final fetched = await fetchData();
+
+        _isComplete = fetched.length < querySize;
+        hasInitialItems = true;
+        _isFetchingData = false;
+
+        setState(() => items.addAll(fetched));
+      } catch (e) {
+        error = e;
+        setState(() {});
+      }
+    });
+
+    scrollController.addListener(
+      () {
+        if (scrollController.position.pixels + 100 >= scrollController.position.maxScrollExtent) {
+          if (!_isFetchingData && !_isComplete) {
+            _isFetchingData = true;
+            Future(() async {
+              try {
+                final fetched = await fetchData();
+
+                _isComplete = fetched.length < querySize;
+                _isFetchingData = false;
+
+                setState(() => items.addAll(fetched));
+              } catch (e) {
+                error = e;
+                setState(() {});
+              }
+            });
+          }
+        }
+      },
+    );
+  }
+}
--- lib/screens/library_screen.dart
+++ lib/screens/library_screen.dart
@@ -1,5 +1,6 @@
 // Project imports:
 import 'package:booklit/booklit.dart';
+import 'package:booklit/core/scroll_page_state.dart';
 
 class LibraryScreen extends StatefulWidget {
   const LibraryScreen({
@@ -9,63 +10,20 @@ class LibraryScreen extends StatefulWidget {
   LibraryScreenState createState() => LibraryScreenState();
 }
 
-class LibraryScreenState extends State<LibraryScreen> {
-  static const _querySize = 50;
+class LibraryScreenState extends ScrollPageState<LibraryScreen, MediaItem> {
+  @override
+  final querySize = 50;
 
-  List<MediaItem> children = [];
-  bool isLoading = true;
-  bool isComplete = false;
   bool gridMode = false;
-  final scrollController = ScrollController();
 
   @override
-  void initState() {
-    super.initState();
-    if (children.isEmpty) {
-      REPOSITORY.data!.getAudiobooks(limit: _querySize, start: 0).then((value) {
-        children.addAll(value);
-        if (value.length < _querySize) isComplete = true;
-        isLoading = false;
-        setState(() {});
-      });
-    }
-
-    // Fetch more data when bottom of page is reached
-    scrollController.addListener(
-      () {
-        if (scrollController.position.pixels + 100 >= scrollController.position.maxScrollExtent) {
-          if (!isLoading && !isComplete) {
-            isLoading = true;
-            REPOSITORY.data!.getAudiobooks(limit: _querySize, start: children.length).then(
-              (value) {
-                children.addAll(value);
-                if (value.length < _querySize) isComplete = true;
-                isLoading = false;
-                setState(() {});
-              },
-            );
-          }
-        }
-      },
-    );
-  }
-
-  Future<void> refresh() async {
-    if (!isLoading) {
-      isLoading = true;
-      REPOSITORY.data!.getAudiobooks(limit: _querySize, start: 0).then((value) {
-        children = value;
-        if (value.length < _querySize) isComplete = true;
-        isLoading = false;
-        setState(() {});
-      });
-    }
-  }
+  Future<List<MediaItem>> fetchData() async =>
+      REPOSITORY.data!.getAudiobooks(limit: querySize, start: itemsFetched);
 
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: refresh,
+      onRefresh: refreshData,
       child: Scrollbar(
         child: CustomScrollView(
           scrollDirection: Axis.vertical,
@@ -73,28 +31,10 @@ class LibraryScreenState extends State<LibraryScreen> {
           slivers: [
             const SliverAppBar(
               title: Text("Library"),
-              //! Grid mode disabled do to GridItem widget bug
-              // actions: [
-              //   IconButton(
-              //       onPressed: () => setState(() => gridMode = !gridMode),
-              //       icon: Icon(gridMode ? Icons.grid_on : Icons.list_rounded)),
-              // ],
+
             ),
-            // if (gridMode)
-            //   SliverPadding(
-            //     padding: EdgeInsets.symmetric(horizontal: 20),
-            //     sliver: SliverGrid(
-            //       gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            //         maxCrossAxisExtent: 250,
-            //         crossAxisSpacing: 20,
-            //         childAspectRatio: 0.7,
-            //       ),
-            //       delegate: SliverChildListDelegate(children.map((e) => GridItem(e)).toList()),
-            //     ),
-            //   )
-            // else
-            SliverList(
-                delegate: SliverChildListDelegate(children.map((e) => ListItem(e)).toList())),
+
+            SliverList(delegate: SliverChildListDelegate(items.map((e) => ListItem(e)).toList())),
             const SliverPadding(
               padding: EdgeInsets.only(bottom: 80),
             )
